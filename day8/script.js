2b4a9566e6b874a1f89035ad6b6800e797209d3c
santja <nastjanas4@gmail.com>
2017-12-08 20:15:30 +0300

    добавили смену фона

--- day8/script.js
+++ day8/script.js
@@ -1,2 +1,12 @@
 // alert('Привет!')
 
+var colorArray = ["#5A9C6E", "#A8BF5A", "#FAC46E", "#FAD5BB", "#F2FEFF"];
+var i = 0;
+
+function changeColor() {
+    document.body.style.background = colorArray[i];
+    i++;
+    if (i > colorArray.length - 1) {
+        i = 0;
+    }
+}
