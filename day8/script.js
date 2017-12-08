65d9e47bdd3e9c8dfe43194c0c6eff388f966d0b
santja <nastjanas4@gmail.com>
2017-12-08 21:30:21 +0300

    генерируем параграфы

--- day8/script.js
+++ day8/script.js
@@ -1,6 +1,6 @@
 // alert('Привет!')
 
-var colorArray = ["#5A9C6E", "#A8BF5A", "#FAC46E", "#FAD5BB", "#F2FEFF"];
+var colorArray = ['#5A9C6E', '#A8BF5A', '#FAC46E', '#FAD5BB', '#F2FEFF'];
 var i = 0;
 
 function changeColor() {
@@ -8,5 +8,19 @@ function changeColor() {
     i++;
     if (i > colorArray.length - 1) {
         i = 0;
+    }
+}
+
+// сгенерить разноцветные параграфы
+
+function genPs() {
+    console.log('tada!')
+    var my_p;
+
+    for (i = 0; i < colorArray.length; i++) {
+        my_p = document.createElement('p');
+        my_p.className = 'stretchy';
+        my_p.style.backgroundColor = colorArray[i];
+        document.body.append(my_p);
     }
 }
